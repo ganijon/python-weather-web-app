cb16f09d0d9a5f874817a9bf56e5f42d9c326a56
Ganijon Rahimov <ganijon.rahimov@se.com>
2019-08-22 11:24:13 -0400

    add html, css, json

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from api_client import weather_api_client
+import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Welcome'
+    return render_template('index.html')
 
-@app.route('/weather')
-def weather():
-    
+
+@app.route('/weather/<city>')
+def weather(city):
+
     wapi = weather_api_client()
-    return wapi.get_weather('Boston,US')
+    # data = wapi.get_weather(city)
+
+    # read file
+    with open('sample_data.json', 'r') as myfile:
+        data = myfile.read()
+
+    return render_template('weather.html', city_name=city, weather_data=json.loads(data))
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
