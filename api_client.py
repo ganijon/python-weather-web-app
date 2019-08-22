e7d4401a417a0599c8bad09cbf7cae2474e86870
Ganijon Rahimov <ganijon.rahimov@se.com>
2019-08-22 11:43:38 -0400

    added forecast

--- api_client.py
+++ api_client.py
@@ -24,3 +24,22 @@ class weather_api_client:
             return json.loads(response.content.decode('utf-8'))
         else:
             return response.content
+
+    def get_forecast(self, city_name):
+        
+        # https://api.openweathermap.org/data/2.5/forecast?q=Boston,US&appid=623af9cc19f6aab782588ed432ed3434
+
+        api_url = '{0}forecast?q={1}&appId={2}'.format(self.api_url_base, city_name, self.api_key)
+                
+        connection = http.client.HTTPSConnection(self.api_url_base, 80, timeout=10)
+              
+        headers = {'Content-Type': 'application/json'}
+                
+        connection.request("GET", api_url, json.dumps(headers))
+
+        response = connection.getresponse()
+
+        if response.status_code == 200:
+            return json.loads(response.content.decode('utf-8'))
+        else:
+            return response.content
--- app.py
+++ app.py
@@ -12,15 +12,30 @@ def index():
 
 @app.route('/weather/<city>')
 def weather(city):
-
     wapi = weather_api_client()
-    # data = wapi.get_weather(city)
+    try:
+        data = wapi.get_weather(city)
+        return render_template('weather.html', city_name=city, weather_data=json.loads(data))
+    except:
+        print('Unable to get weather data. Reading from file...')
+        # read file
+        with open('weather_data.json', 'r') as myfile:
+            data = myfile.read()
+        return render_template('weather.html', city_name=city, weather_data=json.loads(data))
 
-    # read file
-    with open('sample_data.json', 'r') as myfile:
-        data = myfile.read()
 
-    return render_template('weather.html', city_name=city, weather_data=json.loads(data))
+@app.route('/forecast/<city>')
+def forecast(city):
+    wapi = weather_api_client()
+    try:
+        data = wapi.get_forecast(city)
+        return render_template('forecast.html', city_name=city, forecast_data=json.loads(data))
+    except:
+        print('Unable to get forecast data. Reading from file...')
+        # read file
+        with open('forecast_data.json', 'r') as myfile:
+            data = myfile.read()
+        return render_template('forecast.html', city_name=city, forecast_data=json.loads(data))
 
 
 if __name__ == '__main__':
