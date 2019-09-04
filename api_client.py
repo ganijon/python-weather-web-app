e309803367f828be256bc005e3d7d1d524dbc346
Ganijon Rahimov <ganijon.rahimov@se.com>
2019-09-04 14:20:52 -0400

    fixed api client

--- api_client.py
+++ api_client.py
@@ -1,45 +1,20 @@
-import http.client
-import json
+import requests
 
 class weather_api_client:
     def __init__(self):
-        self.api_url_base = 'api.openweathermap.org/data/2.5/'
+        self.url_base = 'api.openweathermap.org/data/2.5/'
         self.api_key = '623af9cc19f6aab782588ed432ed3434'
-    
+
     def get_weather(self, city_name):
-        
-        # https://api.openweathermap.org/data/2.5/weather?q=Boston,US&appid=623af9cc19f6aab782588ed432ed3434
-
-        api_url = '{0}weather?q={1}&appId={2}'.format(self.api_url_base, city_name, self.api_key)
-                
-        connection = http.client.HTTPSConnection(self.api_url_base, 80, timeout=10)
-              
-        headers = {'Content-Type': 'application/json'}
-                
-        connection.request("GET", api_url, json.dumps(headers))
-
-        response = connection.getresponse()
-
+        response = requests.get(f'https://{self.url_base}weather?q={city_name}&appId={self.api_key}')
         if response.status_code == 200:
-            return json.loads(response.content.decode('utf-8'))
+            return response.json()
         else:
-            return response.content
+            return response.text
 
     def get_forecast(self, city_name):
-        
-        # https://api.openweathermap.org/data/2.5/forecast?q=Boston,US&appid=623af9cc19f6aab782588ed432ed3434
-
-        api_url = '{0}forecast?q={1}&appId={2}'.format(self.api_url_base, city_name, self.api_key)
-                
-        connection = http.client.HTTPSConnection(self.api_url_base, 80, timeout=10)
-              
-        headers = {'Content-Type': 'application/json'}
-                
-        connection.request("GET", api_url, json.dumps(headers))
-
-        response = connection.getresponse()
-
+        response = requests.get(f'https://{self.url_base}forecast?q={city_name}&appId={self.api_key}')
         if response.status_code == 200:
-            return json.loads(response.content.decode('utf-8'))
+            return response.json()
         else:
-            return response.content
+            return response.text
